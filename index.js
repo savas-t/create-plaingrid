2d7a5190d206782185eb210cefd7d9b230655058
Savas Tireng <savas-t@outlook.com>
2024-06-16 22:32:53 +0200

    Moved logic into functions

--- index.js
+++ index.js
@@ -1,64 +1,97 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import { writeFileSync } from "fs";
+import inquirer from 'inquirer'
+import { writeFileSync } from 'fs'
 
-/* BASE VARIABLES */
+/**
+ * Returns an array of prompts, ready for inquirer
+ * @param {{base: null, sm: string, md: string, lg: string, xl: string, 2xl: string}} breakpoints
+ * @returns {Array<{type: string, name: string, message: string, default: string}>}
+ */
+const getPrompts = breakpoints => {
+  const prompts = []
 
-const numberOfColumns = 12;
-const breakpoints = {
-  base: null,
-  sm: "640px",
-  md: "768px",
-  lg: "1024px",
-  xl: "1280px",
-  "2xl": "1536px",
-};
+  for (const key in breakpoints) {
+    if (key == 'base') continue
 
-/* LET USER CHOOSE BREAKPOINTS */
-
-const questions = [];
-for (const key in breakpoints) {
-  if (key == "base") continue;
-
-  questions.push({
-    type: "input",
-    name: key,
-    message: `${key}:`,
-    default: breakpoints[key],
-  });
-}
-
-const answers = await inquirer.prompt(questions);
-
-Object.assign(breakpoints, answers);
-
-/* GENERATE CSS CLASSES */
-
-let fileContent = "";
-
-// Add grid gap variable and grid-container class
-fileContent += ":root { --grid-gap: 1rem }\n";
-fileContent += `.grid-container { display: grid; grid-template-columns: repeat(${numberOfColumns}, 1fr); gap: var(--grid-gap) }\n`;
-
-for (const key in breakpoints) {
-  if (key != "base")
-    fileContent += `@media (min-width: ${breakpoints[key]}) {\n`;
-
-  for (let column = 1; column <= numberOfColumns; column++) {
-    // Add colspan and colstart classes
-    fileContent += `.grid-item.colspan-${key}-${column} { grid-column-end: span ${column}; }\n`;
-    fileContent += `.grid-item.colstart-${key}-${column} { grid-column-start: ${column}; }\n`;
+    prompts.push({
+      type: 'input',
+      name: key,
+      message: `${key}:`,
+      default: breakpoints[key],
+    })
   }
 
-  if (key != "base") fileContent += "}\n";
+  return prompts
 }
 
-/* WRITE CLASSES TO FILE */
+/**
+ * Defines a set of default breakpoints and prompts the user for custom values
+ * @returns {{base: null, sm: string, md: string, lg: string, xl: string, 2xl: string}}
+ */
+const promptBreakpoints = async () => {
+  const breakpoints = {
+    base: null,
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1536px',
+  }
 
-try {
-  writeFileSync("./plaingrid.css", fileContent);
-  console.log("\nYour plaingrid.css is ready to go!");
-} catch (error) {
-  console.error(error);
+  const prompts = getPrompts(breakpoints)
+  const answers = await inquirer.prompt(prompts)
+  Object.assign(breakpoints, answers)
+
+  return breakpoints
 }
+
+/**
+ * Generates and returns the CSS code for the grid system
+ * @param {{base: null, sm: string, md: string, lg: string, xl: string, 2xl: string}} breakpoints
+ * @param {number} columns
+ * @returns {string}
+ */
+const getStyles = (breakpoints, columns) => {
+  let styles = ''
+
+  styles += ':root { --grid-gap: 1rem }\n'
+  styles += `.grid-container { display: grid; grid-template-columns: repeat(${columns}, 1fr); gap: var(--grid-gap) }\n`
+
+  for (const key in breakpoints) {
+    if (key != 'base') {
+      styles += `@media (min-width: ${breakpoints[key]}) {\n`
+    }
+
+    for (let column = 1; column <= columns; column++) {
+      styles += `.grid-item.colspan-${key}-${column} { grid-column-end: span ${column}; }\n`
+      styles += `.grid-item.colstart-${key}-${column} { grid-column-start: ${column}; }\n`
+    }
+
+    if (key != 'base') {
+      styles += '}\n'
+    }
+  }
+
+  return styles
+}
+
+/**
+ * Writes CSS code to a plaingrid.css file
+ * @param {string} styles
+ */
+const writeFile = styles => {
+  try {
+    writeFileSync('./plaingrid.css', styles)
+    console.log('\nYour plaingrid.css is ready to go!')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/** PROGRAM */
+const columns = 12
+const breakpoints = await promptBreakpoints()
+const styles = getStyles(breakpoints, columns)
+
+writeFile(styles)
